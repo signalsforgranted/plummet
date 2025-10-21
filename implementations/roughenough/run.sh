eb1d699671727894da33b74f38b888c767685447
Sarah Grant <sarah.grant.ietf@gmail.com>
2025-10-21 18:37:25 +0000

    Updating roughenough to latest version
    This also now runs as -14, which may break with others, let's see...

--- implementations/roughenough/run.sh
+++ implementations/roughenough/run.sh
@@ -3,9 +3,9 @@
 MODE=$1
 
 if [[ "$MODE" == "client" ]]; then
-    sleep 2 && /target/release/roughenough-client -d -p 11 roughtime-server 2002 &> /data/client.log;
+    sleep 2 && /target/release/roughenough_client -v -P 14 roughtime-server 2002 &> /data/client.log;
 elif [[ "$MODE" == "server" ]]; then
-    /target/release/roughenough-server ENV &> /data/server.log;
+    /target/release/roughenough_server ENV &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
