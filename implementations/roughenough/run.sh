4268b13f2d5c13ed90385ccebdb8118673380491
Sarah Grant <sarah.grant.ietf@gmail.com>
2025-11-07 14:17:42 +0000

    Attempting to fix roughenough's arguments

--- implementations/roughenough/run.sh
+++ implementations/roughenough/run.sh
@@ -3,9 +3,9 @@
 MODE=$1
 
 if [[ "$MODE" == "client" ]]; then
-    sleep 2 && /target/release/roughenough_client -v -P 14 roughtime-server 2002 &> /data/client.log;
+    sleep 2 && /target/release/roughenough_client -v roughtime-server 2002 &> /data/client.log;
 elif [[ "$MODE" == "server" ]]; then
-    /target/release/roughenough_server --verbose &> /data/server.log;
+    /target/release/roughenough_server -v &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
