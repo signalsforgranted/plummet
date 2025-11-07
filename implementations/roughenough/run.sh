351284afbb222c7948b9487e4a02a2eb386dc48e
Sarah Grant <sarah.grant.ietf@gmail.com>
2025-11-07 13:37:49 +0000

    Updating roughenough, fixing run script
    Client appears to remain broken due to what appears to be container DNS things.

--- implementations/roughenough/run.sh
+++ implementations/roughenough/run.sh
@@ -5,7 +5,7 @@ MODE=$1
 if [[ "$MODE" == "client" ]]; then
     sleep 2 && /target/release/roughenough_client -v -P 14 roughtime-server 2002 &> /data/client.log;
 elif [[ "$MODE" == "server" ]]; then
-    /target/release/roughenough_server ENV &> /data/server.log;
+    /target/release/roughenough_server --verbose &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
